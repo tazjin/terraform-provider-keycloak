e64bf41626367372144f2816be5b92a58e1808ff
Vincent Ambo <tazjin@gmail.com>
2017-05-25 14:43:20 +0200

    refactor provider: Look up user roles by ID, too
    This should make it possible to import user role mappings.

--- keycloak/role_mappings.go
+++ keycloak/role_mappings.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Role struct {
-	Id    string `json:"id"`
-	Name  string `json:"name,omitempty"`
-	Realm string `json:"containerId,omitempty"`
+	Id                 string `json:"id"`
+	Name               string `json:"name,omitempty"`
+	ScopeParamRequired bool   `json:"scopeParamRequired"`
 }
 
 const (
@@ -39,16 +39,16 @@ func (c *KeycloakClient) GetCompositeRolesForUser(userId string, realm string) (
 // Find a role for a given user based on the role ID.
 // The idea is to hide the complexity of the randomly generated role IDs from the user.
 // TODO: Evaluate whether this is the most sensible approach vs. some sort of data provider
-func (c *KeycloakClient) FindRoleForUser(roles []Role, roleName string) (*Role, error) {
+func (c *KeycloakClient) FindRoleForUser(roles []Role, roleIdentifier string) (*Role, error) {
 	var role Role
 	for _, value := range roles {
-		if value.Name == roleName {
+		if value.Name == roleIdentifier || value.Id == roleIdentifier {
 			role = value
 		}
 	}
 
 	if role.Id == "" {
-		return nil, fmt.Errorf("Role %s not found", roleName)
+		return nil, fmt.Errorf("Role %s not found", roleIdentifier)
 	}
 
 	return &role, nil
